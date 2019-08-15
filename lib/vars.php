45be5f9acb4aab8b9c3e0ee53a717ec10da67290
Atakan Çelik <37778749+atakancelik@users.noreply.github.com>
2019-08-15 22:12:53 +0300

    Update vars.php

--- lib/vars.php
+++ lib/vars.php
@@ -1,4 +1,4 @@
 <?php
 
-$amp_c_u_project_path = "amp-cache-update";
+$amp_c_u_project_path = "wp-amp-onbellek-guncelleme-eklentisi-master";
 define( "AMP_C_U_PLUGIN_PATH", WP_PLUGIN_DIR."/".$amp_c_u_project_path );
